4b29d9ab29fd7528a2c3fb66ace433fd89cde17a
imbanghee <imbanghee@gmail.com>
2018-01-06 00:31:52 +0900

    1월6일 로컬홈작업

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,22 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,8 +32,47 @@ public class RequestHandler extends Thread {
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			log.debug("request line : {}",line);
+			String[] tokens = line.split(" ");
+			for (String token : tokens) {
+				log.debug("token : {}",token);
+			}
+			String url = tokens[1];
+			int index = url.indexOf("?");
+			if(index != -1) {
+				String requestPath = url.substring(0, index);
+				String params = url.substring(index+1);
+				log.debug("params : {}",params);
+				
+				Map<String,String> param = HttpRequestUtils.parseQueryString(params);
+				
+				User user = new User(param.get("userId"), param.get("password"), param.get("name"), param.get("email"));
+				log.debug("userinfo : {}",user.toString());
+			}
+			
+			Map<String, String> headers = new HashMap<String, String>();
+			while(!"".equals(line)) {
+				line = br.readLine();
+				log.debug("header : {}",line);
+				String[] headerTokens = line.split(": ");
+				if(headerTokens.length ==2) {
+					headers.put(headerTokens[0], headerTokens[1]);
+				}
+			}
+			
+			if(url.startsWith("/create")) {
+				String contentLength = headers.get("Content-Length");
+				String requestBody = util.IOUtils.readData(br, Integer.parseInt(contentLength));
+				log.debug("requestBody : {}",requestBody);
+			}
+			
+			String filePath = "./webapp"+url;
+			byte[] body = Files.readAllBytes(new File(filePath).toPath());
+			
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+//			byte[] body = "Hello World".getBytes();
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
